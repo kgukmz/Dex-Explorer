7da205fe72b72066cf106ed11c5839a24d9866d0
kgukmz <144381271+kgukmz@users.noreply.github.com>
2023-11-19 20:23:47 +0000

    Update Dex-Main.lua

--- Dex-Main.lua
+++ Dex-Main.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 CreateGui = function()
 NewGuiPart1 = Instance.new("ScreenGui")
 local NewGuiPart2 = Instance.new("Frame")
